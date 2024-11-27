9ff08258f319d19c1b2418604eb08391aa56083c
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-11-27 12:07:08 +0100

    add provider

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,31 @@ import AboutPage from './pages/AboutPage';
 import PostPage from './pages/PostPage';
 import NotFoundPage from './pages/NotFoundPage';
 
+// context
+import GlobalContext from "./contexts/GlobalContext.jsx"
+import PostsList from './components/Posts/PostsList.jsx';
+
 
 function App() {
 
   // render
   return (
     <>
+      <GlobalContext.Provider value={{ PostsList }}>
+        <BrowserRouter>
+          <Routes>
 
-      <BrowserRouter>
-        <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route path='/' element={<HomePage />}></Route>
+              <Route path='/posts' element={<PostsPage />}></Route>
+              <Route path='/posts/:slug' element={<PostPage />}></Route>
+              <Route path='/about' element={<AboutPage />}></Route>
+              <Route path='*' element={<NotFoundPage />}></Route>
 
-          <Route element={<DefaultLayout />}>
-            <Route path='/' element={<HomePage />}></Route>
-            <Route path='/posts' element={<PostsPage />}></Route>
-            <Route path='/about' element={<AboutPage />}></Route>
-            <Route path='/posts/:slug' element={<PostPage />}></Route>
-            <Route path='*' element={<NotFoundPage />}></Route>
-
-          </Route>
-        </Routes>
-      </BrowserRouter>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </GlobalContext.Provider >
     </>
   );
 };
